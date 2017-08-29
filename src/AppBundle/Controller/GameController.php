a34f9a3b99972ae93c1af0c3ca57a10742ccafc6
Jarrett Barnett <git@jarrettbarnett.com>
2017-08-29 12:42:41 -0700

    Implemented data storage for game round results

--- src/AppBundle/Controller/GameController.php
+++ src/AppBundle/Controller/GameController.php
@@ -2,9 +2,15 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\App\Round;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Jarrett\RockPaperScissorsSpockLizard\Game;
+use Jarrett\RockPaperScissorsSpockLizard\Player;
+use Jarrett\RockPaperScissorsSpockLizardException;
 
 class GameController extends Controller
 {
@@ -13,18 +19,65 @@ class GameController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $move = new Move();
+        $round = new Round();
 
-        $form = $this->createFormBuilder($move)
-            ->add('move', TextType::class)
-            ->createForm();
+        $options['label'] = 'Make A Move';
+        $options['choices'] = [
+            'Rock' => 'rock',
+            'Paper' => 'paper',
+            'Scissors' => 'scissors',
+            'Spock' => 'spock',
+            'Lizard' => 'lizard'
+        ];
+
+        $form = $this->createFormBuilder($round)
+            ->add('move', ChoiceType::class, $options)
+            ->add('submit', SubmitType::class)
+            ->getForm();
 
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $play = $form->getData();
+            $data = $form->getData();
+
+            try {
+                // create player
+                $player = new Player('Human');
+                $player->move($data->getMove());
+
+                // create bot
+                $bot = new Player('Bot');
+                $bot->isBot(true);
+
+                // setup game
+                $game = new Game();
+                $outcome = $game->addPlayers($player, $bot)
+                    ->play()
+                    ->getRoundOutcome();
+
+                // get bot's move
+                $bot_move = $bot->getLastMove();
+                $data->setOpponentMove($bot_move);
+
+                // lookup outcome
+                if (!empty($outcome['ties'][0])) {
+                    $tie = $outcome['ties'][0];
+                    $data->setOutcome('tied');
+                    $data->setOutcomeDescription($tie['description']);
+
+                } else {
+                    $player = $outcome['winners'][0]['player'];
+                    $result = $player->getName() === 'Human' ? 'won' : 'lost';
+                    $data->setOutcome($result);
+                    $data->setOutcomeDescription($outcome['winners'][0]['description']);
+                }
+
+            } catch (RockPaperScissorsSpockLizardException $e) {
+                // TODO display flash message
+            }
+
             $doctrine = $this->getDoctrine()->getManager();
-            $doctrine->persist($play);
+            $doctrine->persist($data);
             $doctrine->flush();
 
             return $this->redirectToRoute('homepage');
@@ -37,5 +90,4 @@ class GameController extends Controller
 
         return $this->render('game/index.html.twig', $view_data);
     }
-
 }
