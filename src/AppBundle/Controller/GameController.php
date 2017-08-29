b33e3fb959fea9b5ca422cd674a016fc50e4d5c5
Jarrett Barnett <git@jarrettbarnett.com>
2017-08-29 14:16:58 -0700

    Numerous styling and view data improvements

--- src/AppBundle/Controller/GameController.php
+++ src/AppBundle/Controller/GameController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\App\Round;
+use AppBundle\Repository\App\RoundRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -21,22 +22,31 @@ class GameController extends Controller
     {
         $round = new Round();
 
-        $options['label'] = 'Make A Move';
-        $options['choices'] = [
-            'Rock' => 'rock',
-            'Paper' => 'paper',
-            'Scissors' => 'scissors',
-            'Spock' => 'spock',
-            'Lizard' => 'lizard'
-        ];
-
         $form = $this->createFormBuilder($round)
-            ->add('move', ChoiceType::class, $options)
-            ->add('submit', SubmitType::class)
+            ->add('move', ChoiceType::class, [
+                'label' => 'Make A Move!',
+                'label_attr' => ['class' => 'display-3'],
+                'choices' => [
+                    'Rock' => 'rock',
+                    'Paper' => 'paper',
+                    'Scissors' => 'scissors',
+                    'Spock' => 'spock',
+                    'Lizard' => 'lizard'
+                ],
+                'attr' => ['class' => 'display-4']
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Submit Move!',
+                'attr' => ['class' => 'btn btn-lg btn-success'],
+            ])
             ->getForm();
+
+        $doctrine = $this->getDoctrine();
+        $doctrineManager = $doctrine->getManager();
 
         $form->handleRequest($request);
 
+        // handle player move
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
 
@@ -76,16 +86,20 @@ class GameController extends Controller
                 // TODO display flash message
             }
 
-            $doctrine = $this->getDoctrine()->getManager();
-            $doctrine->persist($data);
-            $doctrine->flush();
+            $doctrineManager->persist($data);
+            $doctrineManager->flush();
 
             return $this->redirectToRoute('homepage');
         }
 
+        $latest_rounds = $doctrine
+            ->getRepository(Round::class)
+            ->findAll();
+
         $view_data = [
-            'pageTitle' => 'Make a move',
-            'form' => $form->createView()
+            'pageTitle' => 'Rock-Paper-Scissors-Spock-Lizard!',
+            'form' => $form->createView(),
+            'statistics' => $latest_rounds
         ];
 
         return $this->render('game/index.html.twig', $view_data);
